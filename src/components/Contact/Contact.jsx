b00cfaf3b7e642b61cb57f63dbcd885140b16f1d
ankhod <ankhod7@gmail.com>
2025-03-09 00:57:05 +0200

    test-2-icons

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,17 @@
+import { FaUser, FaPhone } from 'react-icons/fa'; // Імпортуємо іконки
 import styles from './Contact.module.css';
 
 export default function Contact({ id, name, number, onDelete }) {
   return (
     <li className={styles.contact}>
-      <span>
-        {name}: {number}
-      </span>
+      <div className={styles.info}>
+        <span className={styles.name}>
+          <FaUser className={styles.icon} /> {name}
+        </span>
+        <span className={styles.number}>
+          <FaPhone className={styles.icon} /> {number}
+        </span>
+      </div>
       <button onClick={() => onDelete(id)} className={styles.delete}>
         Delete
       </button>
